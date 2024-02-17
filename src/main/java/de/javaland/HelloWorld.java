b9ce9024ccd7c08bdddfcc85b391ebe95ad44b87
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2024-02-17 16:08:35 +0100

    Initial commit

---
+++ src/main/java/de/javaland/HelloWorld.java
@@ -0,0 +1,21 @@
+package de.javaland;
+
+public class HelloWorld {
+
+    private static final String DEFAULT_VALUE = "World";
+
+    public static void main(String[] args) {
+        String message = createMessage(args);
+        System.out.println(message);
+    }
+
+    private static String createMessage(String[] args) {
+        var name = DEFAULT_VALUE;
+
+        if (args.length != 0) {
+            name = args[0];
+        }
+
+        return String.format("Hello %s!", name);
+    }
+}
